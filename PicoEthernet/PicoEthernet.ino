62786fc4e789bc029219589c53f15afbf890f895
j-exinia <exinia@ou.edu>
2023-02-13 15:06:19 -0600

    Test code for CAN and Ethernet working simultaneously

--- PicoEthernet/PicoEthernet.ino
+++ PicoEthernet/PicoEthernet.ino
@@ -17,97 +17,153 @@
 
  */
 
-#include <SPI.h>
 #include <Ethernet.h>
 #include <EthernetUdp.h>
+#include <SPI.h>
+#include "TickTwo.h" //library to handle timing processes
+#include <ACAN2515.h>
 
-// Enter a MAC address for your controller below.
-// Newer Ethernet shields have a MAC address printed on a sticker on the shield
+
 byte mac[] = {
-  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED  // sets MAC address of MCU
 };
+IPAddress ip(192, 168, 1, 3);       // sets the IP of the MCU
 
-unsigned int localPort = 1003;       // local port to listen for UDP packets
+unsigned int localPort = 1003;      // local port to listen on
 
-// const char timeServer[] = "time.nist.gov"; // time.nist.gov NTP server
-const char timeServer[] = "192.168.1.1"; // my mac address i was trying to set up
+// buffers for receiving and sending data
+char packetBuffer[UDP_TX_PACKET_MAX_SIZE];  // buffer to hold incoming packet,
+char ReplyBuffer[] = "acknowledged";        // a string to send back
 
-const int NTP_PACKET_SIZE = 1; // NTP time stamp is in the first 48 bytes of the message
-
-byte packetBuffer[NTP_PACKET_SIZE]; //buffer to hold incoming and outgoing packets
-
-// A UDP instance to let us send and receive packets over UDP
+// An EthernetUDP instance to let us send and receive packets over UDP
 EthernetUDP Udp;
 
-void setup() {
-  // You can use Ethernet.init(pin) to configure the CS pin
-  //Ethernet.init(10);  // Most Arduino shields
-  //Ethernet.init(5);   // MKR ETH Shield
-  //Ethernet.init(0);   // Teensy 2.0
-  //Ethernet.init(20);  // Teensy++ 2.0
-  //Ethernet.init(15);  // ESP8266 with Adafruit FeatherWing Ethernet
-  //Ethernet.init(33);  // ESP32 with Adafruit FeatherWing Ethernet
-  Ethernet.init(17);    //Pico ethernet shield
+// CAN pic configs
+static const byte MCP2515_SCK  = 10 ; // SCK input of MCP2515
+static const byte MCP2515_MOSI = 11; // SDI input of MCP2515
+static const byte MCP2515_MISO = 8 ; // SDO output of MCP2515
 
-  // Open serial communications and wait for port to open:
+static const byte MCP2515_CS  = 9 ;  // CS input of MCP2515
+static const byte MCP2515_INT = 7 ;  // INT output of MCP2515
+
+ACAN2515 can (MCP2515_CS, SPI1, MCP2515_INT) ; // CAN Uses SPI 1
+static const uint32_t QUARTZ_FREQUENCY = 8UL * 1000UL * 1000UL ; // 8 MHz 
+
+CANMessage frame; //buffer for CAN frame
+
+void configureCan(); 
+void configureEthernet();
+void printCanMsg();
+
+
+void setup() {
+
   Serial.begin(9600);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
-
-  // start Ethernet and UDP
-  if (Ethernet.begin(mac) == 0) {
-    Serial.println("Failed to configure Ethernet using DHCP");
-    // Check for Ethernet hardware present
-    if (Ethernet.hardwareStatus() == EthernetNoHardware) {
-      Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
-    } else if (Ethernet.linkStatus() == LinkOFF) {
-      Serial.println("Ethernet cable is not connected.");
-    }
-    // no point in carrying on, so do nothing forevermore:
-    while (true) {
-      delay(1);
-    }
-  }
-  Udp.begin(localPort);
+  configureEthernet();
+  configureCan();
 }
 
 void loop() {
+  // if there's data available, read a packet
+  int packetSize = Udp.parsePacket();
+  if (packetSize) {
+    Serial.print("Received packet of size ");
+    Serial.println(packetSize);
+    Serial.print("From ");
+    IPAddress remote = Udp.remoteIP();
+    for (int i=0; i < 4; i++) {
+      Serial.print(remote[i], DEC);
+      if (i < 3) {
+        Serial.print(".");
+      }
+    }
+    Serial.print(", port ");
+    Serial.println(Udp.remotePort());
 
-  // wait to see if a reply is available
-  delay(100);
-  if (Udp.parsePacket()) {
-    // We've received a packet, read the data from it
-    Udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
+    // read the packet into packetBuffer
+    Udp.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);
+    Serial.println("Contents:");
+    Serial.println(packetBuffer);
 
 
-    unsigned long word = word(packetBuffer[0], packetBuffer[1]);
-    Serial.println(word);
+    if(packetBuffer[0] == 1){
+      Serial.println("Address: 1");
+    }
+    if (packetBuffer[1] == 65) {
+      Serial.println("Contents:");
+      Serial.println("65 received via UDP!");
+    }
+
+    // send a reply to the IP address and port that sent us the packet we received
+    Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+    Udp.write(ReplyBuffer);
+    Udp.endPacket();
   }
-  // wait ten seconds before asking for the time again
-  delay(100);
-  Ethernet.maintain();
+  delay(10);
 }
 
-// send an NTP request to the time server at the given address
-void sendNTPpacket(const char * address) {
-  // set all bytes in the buffer to 0
-  memset(packetBuffer, 0, NTP_PACKET_SIZE);
-  // Initialize values needed to form NTP request
-  // (see URL above for details on the packets)
-  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
-  packetBuffer[1] = 0;     // Stratum, or type of clock
-  packetBuffer[2] = 6;     // Polling Interval
-  packetBuffer[3] = 0xEC;  // Peer Clock Precision
-  // 8 bytes of zero for Root Delay & Root Dispersion
-  packetBuffer[12]  = 49;
-  packetBuffer[13]  = 0x4E;
-  packetBuffer[14]  = 49;
-  packetBuffer[15]  = 52;
+void configureCan(){
+  SPI1.setSCK (MCP2515_SCK);
+  SPI1.setTX (MCP2515_MOSI);
+  SPI1.setRX (MCP2515_MISO);
+  SPI1.setCS (MCP2515_CS); 
+  SPI1.begin();
 
-  // all NTP fields have been given values, now
-  // you can send a packet requesting a timestamp:
-  Udp.beginPacket(address, 123); // NTP requests are to port 123
-  Udp.write(packetBuffer, NTP_PACKET_SIZE);
-  Udp.endPacket();
+
+  Serial.println ("Configure ACAN2515") ;
+  ACAN2515Settings settings (QUARTZ_FREQUENCY, 100UL * 1000UL) ; // CAN bit rate 100 kb/s
+  const uint16_t errorCode = can.begin (settings,  canISR) ;
+  if(errorCode == 0){
+    Serial.print ("Actual bit rate: ") ;
+    Serial.print (settings.actualBitRate ()) ;
+  }
+
+
+} 
+
+void configureEthernet(){
+  //init pin on Ethernet Shield
+  Ethernet.init(17);
+
+  // start the Ethernet
+  Ethernet.begin(mac, ip);
+
+  // Check for Ethernet hardware present
+  if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+    Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
+    while (true) {
+      delay(1); // do nothing, no point running without Ethernet hardware
+    }
+  }
+  if (Ethernet.linkStatus() == LinkOFF) {
+    Serial.println("Ethernet cable is not connected.");
+  }
+
+  // start UDP
+  Udp.begin(localPort);
 }
+
+//handle and updates values when/if a can message is recieved
+void canISR(){
+  can.isr();
+  can.receive(frame);
+
+  printCanMsg(frame);
+
+}
+
+void printCanMsg(CANMessage frame){
+    Serial.print ("  id: ");Serial.println (frame.id,HEX);
+    Serial.print ("  ext: ");Serial.println (frame.ext);
+    Serial.print ("  rtr: ");Serial.println (frame.rtr);
+    Serial.print ("  len: ");Serial.println (frame.len);
+    Serial.print ("  data: ");
+    for(int x=0;x<frame.len;x++) {
+      Serial.print (frame.data[x],HEX); Serial.print(":");
+    }
+    Serial.println ("");
+}
+
