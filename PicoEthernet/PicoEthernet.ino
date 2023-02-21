4f80b3c5a4baed2aacc3d2157af67ebd261ef7fb
j-exinia <exinia@ou.edu>
2023-02-20 18:44:04 -0600

    deleted example credentials

--- PicoEthernet/PicoEthernet.ino
+++ PicoEthernet/PicoEthernet.ino
@@ -1,22 +1,3 @@
-/*
- Udp NTP Client
-
- Get the time from a Network Time Protocol (NTP) time server
- Demonstrates use of UDP sendPacket and ReceivePacket
- For more on NTP time servers and the messages needed to communicate with them,
- see https://en.wikipedia.org/wiki/Network_Time_Protocol
-
- created 4 Sep 2010
- by Michael Margolis
- modified 9 Apr 2012
- by Tom Igoe
- modified 02 Sept 2015
- by Arturo Guadalupi
-
- This code is in the public domain.
- 
- */
-
 #include <Ethernet.h>
 #include <EthernetUdp.h>
 #include "TickTwo.h" //library to handle timing processes
